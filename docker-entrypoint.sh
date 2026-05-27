a317fcc221f8250e30cfd0e812187386311ec243
ArangXG <arangxg@gmail.com>
2026-05-27 14:45:11 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Default values — override dengan -e saat docker run
 ALGO="${ALGO:-pearl}"
 POOL="${POOL:-stratum+tcp://pearl-eu1.luckypool.io:3360}"
-WALLET="${WALLET:-prl1pvxf2ljgw6xw32fzwjftt660m7jny6hl2lp7n5c3dq6w5a8maekpqwjpge8.sph100x1}"
+WALLET="${WALLET:-prl1pasdasdasd.worker}"
 WORKER="${WORKER:-rig1}"
 
 echo "========================================="
